8341e06f87562ad70318b8c9281d620ab021129e
A A Z <asma.a.z.asma@gmail.com>
2024-08-27 22:09:17 +0300

    chapter 6 adding filter group by and search

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,8 +2,9 @@
     'name': 'Real Estate',
     'author': 'Asmaa AZ',
     'data' : ['security/ir.model.access.csv',
+              'views/estate_property_views.xml',
               'views/estate_menus.xml',
-              'views/estate_property_views.xml',],
+              ],
     'depends':['base'],
     'application': True,
 }
