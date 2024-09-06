70f2da7b6c9f12bfbb9c16e1c8a7b825b9fe04ef
A A Z <asma.a.z.asma@gmail.com>
2024-09-06 13:31:48 +0300

    chapter 8

--- estate/model/estate_property.py
+++ estate/model/estate_property.py
@@ -1,6 +1,6 @@
 from email.policy import default
 
-from odoo import models, fields
+from odoo import models, fields, api
 from datetime import datetime, timedelta
 
 
@@ -37,7 +37,32 @@ class EstateProperty(models.Model):
                              default='new')
 
     property_type_id = fields.Many2one('estate.property.type', string = 'Property Type')
-    salesperson = fields.Many2one('res.users', string = 'Salesman')
+    salesperson = fields.Many2one('res.users', string = 'Salesman', default = lambda self: self.env.user)
     buyer = fields.Many2one('res.partner', string = 'Buyer')
     tag_ids = fields.Many2many('estate.property.tag', string = 'tags')
     offer_ids = fields.One2many('estate.property.offer','property_id', string = 'offers')
+
+    total_area = fields.Integer(compute='_compute_total_area')
+    best_price = fields.Float(compute= '_compute_best_price')
+
+    @api.depends('garden_area', 'living_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_price = 0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ''
--- estate/model/estate_property_offer.py
+++ estate/model/estate_property_offer.py
@@ -1,4 +1,8 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import datetime, timedelta
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -10,3 +14,18 @@ class EstatePropertyOffer(models.Model):
                               copy = False, )
     partner_id = fields.Many2one('res.partner', required = True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute='_compute_date_deadline', default = datetime.today(), inverse = '_inverse_compute_date_deadline')
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = datetime.today() + timedelta(days=record.validity)
+
+    def _inverse_compute_date_deadline(self):
+        for record in self:
+            a = str(record.date_deadline)
+            b = str(datetime.today())
+            _logger.info(a)
+            _logger.info(b)
+            record.validity = a - b
